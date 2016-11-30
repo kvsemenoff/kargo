92b6088ed8cd9da36f3043c7ba17a801d409b541
engine77 <zaur.ka.00@mail.ru>
2016-11-30 17:48:39 +0300

    29

--- includes/timekargo.php
+++ includes/timekargo.php
@@ -1,1 +1,56 @@
-Карго экономит время
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<div class="df-timekargo">
+	<h2>КаргоКод экономит время и деньги</h2>
+	<div class="df-block1">
+		<h4>ЗАДАЧИ</h4>
+		<div class="dfbox">
+			<img src="img/df-img1.png" alt="">
+			<span>Подбор кода по описанию
+				ТН ВЭД ЕАЭС и получение
+				ставки по коду
+			</span>
+		</div>
+		<div class="dfbox">
+			<img src="img/df-img2.png" alt="">
+			<span>Оперативное отслеживание
+				изменений кодов ТН ВЭД ЕАЭС
+				и актуальных ставок
+			</span>
+		</div>
+		<div class="dfbox">
+			<img src="img/df-img3.png" alt="">
+			<span>Работа с большим количеством
+				кодов ТН ВЭД ЕАЭС
+			</span>
+		</div>
+		<div class="clear"></div>
+	</div>
+	<div class="df-block2">
+		<h4>с  КаргоКодом</h4>
+				<span>Моментальный подбор
+		кода ТН ВЭД ЕАЭС</span>
+				<span class="dfpadright">Ручное отслеживание кодов
+		ТН ВЭД ЕАЭС и ставок по ним
+		занимает очень много времени</span>
+				<span class="dfpadright">Ручная простановка актуальных
+		ставок по кодам ТН ВЭД ЕАЭС
+		занимает очень много времени</span>
+	</div>
+	<div class="df-block3">
+		<h4>без  КаргоКода</h4>
+				<span>Подбор кода ТН ВЭД ЕАЭС
+		по описанию занимает
+		очень много времени</span>
+				<span class="dfpadright">Ручное отслеживание кодов
+		ТН ВЭД ЕАЭС и ставок по ним
+		занимает очень много времени</span>
+				<span class="dfpadright">Ручная простановка актуальных
+		ставок по кодам ТН ВЭД ЕАЭС
+		занимает очень много времени</span>
+	</div>
+</div>
+</div>
+</div>
+</div>
