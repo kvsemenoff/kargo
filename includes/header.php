ae09132f58384871154e6f2b2ae684889842a1b0
Berzeg07 <berezg86@gmail.com>
2016-11-30 16:02:04 +0300

    29.11

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<div class="col-md-12">
+	<div class="test">
+		<input class="input-class" placeholder="Инпут" type="text">
+		<a class="button1" href="#">Попробовать бесплатно</a>
+		<label>
+			<input class="checkbox" type="checkbox" name="checkbox-test" checked>
+			<span class="checkbox-custom"></span>
+			<span class="label"></span>
+		</label>
+	</div>
+</div>
