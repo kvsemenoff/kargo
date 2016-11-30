8712189789d29a91aeddbb90e119b4b3ee181309
Barasbi <betokov93@gmail.com>
2016-11-30 17:24:27 +0300

    add db-capability.php and style2.css

--- includes/db-capability.php
+++ includes/db-capability.php
@@ -4,18 +4,17 @@
 			<h2>Что такое КаргоКод?</h2>
 				<div class="db-capability-inner">
 					<div class="db-capability-inner_left">
-					
-					<img src="img/db-computer.png" alt="computer">
+						<img src="img/db-computer.png" alt="computer" class="img-responsive">
 					</div>
 					<div class="db-capability-inner_right">
-					<p>
-					Уникальный алгоритм быстрого <br>
-					подбора кода ТН ВЭД ЕАЭС по <br>
-					поисковой фразе с использованием <br>
-					технологии нейроновых сетей
-					</p>
+						<p>
+						Уникальный алгоритм быстрого <br>
+						подбора кода ТН ВЭД ЕАЭС по <br>
+						поисковой фразе с использованием <br>
+						технологии нейроновых сетей
+						</p>
 					</div>
-			
+			<div class="clearfix"></div>
 				</div>
 		</div>
 	</div>
