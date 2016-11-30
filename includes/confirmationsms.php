9098dc28db2eef6e464e57433f82e58670121a75
unknown <anzarsh@mail.ru>
2016-11-30 17:41:44 +0300

    add second font

--- includes/confirmationsms.php
+++ includes/confirmationsms.php
@@ -1,1 +1,18 @@
-Подтверждение по СМС
+<div class="de-confirm_by_sms">
+	<div class="line1">
+		<span class="caption">
+			Подтверждение по СМС
+		</span>
+	</div>	
+	<span class="caption2">
+		На указанный Вами телефон +79261234567 выслан код для подтверждения.
+	</span>
+	<div class="line3">
+		<span class="txt"></span>
+		<input type="text" value="">
+	</div>
+	<div class="line4">
+		<span class="cpt"><a href="#">Выслать код повторно</a></span>
+	</div>
+	<a href="" class="registration">Зарегистрироваться</a>	
+</div>
--- index.php
+++ index.php
@@ -34,7 +34,7 @@
 
 	<?php require_once('includes/header.php'); ?>
 	<?php require_once('includes/db-capability.php'); ?>
-	
+	<?php require_once('includes/whatcancargo.php'); ?>	
 	<?php require_once('includes/whoneedkargo.php'); ?>
 	<?php require_once('includes/timekargo.php'); ?>
 	<?php require_once('includes/ekonomy95.php'); ?>
