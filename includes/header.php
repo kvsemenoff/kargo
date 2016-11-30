807fb1bfca1ff2d113b3f214ba8c8960eb1d4ece
Berzeg07 <berezg86@gmail.com>
2016-11-30 16:55:41 +0300

    29.11

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,17 @@
-История запросов
+<div class="anz-menu">
+	    <input type="checkbox" id="check_1" class=""/>
+	    <label class="anz-menu-ch hidden-lg hidden-md" for="check_1"><i class="fa fa-bars" aria-hidden="true"></i><img src="img/menu-ico.png"></label>
+	    <div class="anz-perspective">
+	        <div class="menu-list-pressed">
+	            <label class="anz-menu-ch1" for="check_1"><i class="fa fa-times" aria-hidden="true"></i></label>
+	            <ul class="dd-mainmenu">
+					<li><a href="#">Что такое карго код?</a></li>
+					<li><a href="#">Кому нужен карго код?</a></li>
+					<li><a href="#">Возможности</a></li>
+					<li><a href="#">Стоимость</a></li>
+				</ul>
+	        </div>
+	    </div>
+	</div>
+</div>
+
