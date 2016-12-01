e739e8224778ad26e28d415ec4d10b2d58aab30f
unknown <anzarsh@mail.ru>
2016-12-01 13:07:16 +0300

    userprofile.php

--- includes/userprofile.php
+++ includes/userprofile.php
@@ -1,1 +1,38 @@
-Профиль пользователя
+<div class="userprofile">
+	<div class="up-caption">
+		<span class="caption">
+			Профиль пользователя <span class="caption_green">testuser</span>
+		</span>
+	</div>
+	<div class="up-form">
+		<div class="ln1">
+			<span class="label1">Логин:</span>
+			<input class="input-class btn-for-label1" type="text" value="testuser">
+		</div>
+		<div class="ln2">
+			<span class="label2">E-Mail:</span>
+			<input class="input-class btn-for-label2" type="text" value="testuser@testuser.com">
+		</div>
+		<div class="ln3">
+			<span class="label3">Телефон:</span>
+			<input class="input-class btn-for-label3" type="text" value="+79261234567">
+		</div>
+		<div class="ln4">
+			<span class="label4">Пароль:</span>
+			<a href="#" class="chpass_link"><span class="chpass_caption">Сменить пароль</span></a>
+		</div>
+		<div class="ln5">
+
+			<label>
+				<input class="checkbox" type="checkbox" name="checkbox-test" checked>
+				<span class="checkbox-custom"></span>
+				<span class="label check_caption">Получать новости сервиса</span>
+			</label>
+
+		</div>
+		<div class="ln6">
+			<a href="#" class="button1 save">Сохранить</a>
+			<a href="#" class="button1 cancel">Отменить</a>
+		</div>
+	</div>
+</div>
