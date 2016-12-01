103ec5bb336deb1b3c24e2cca035a08cae6ac33b
Berzeg07 <berezg86@gmail.com>
2016-12-01 13:41:31 +0300

    29.11

--- includes/footer-processing.php
+++ includes/footer-processing.php
@@ -12,21 +12,22 @@
 				</div>
 				<div class="dd-footer-txt dd-footer-txt2">
 					<ul>
-						<li><a href="#">Что такое КаргоКод?</a></li>
-						<li><a href="#">Кому нужен КаргоКод</a></li>
+						<li><a href="#">Главная</a></li>
+						<li><a href="#">Подбор кода</a></li>
+						<li><a href="#">История запросов</a></li>
 					</ul>
 				</div>
 				<div class="dd-footer-txt">
 					<ul>
-						<li><a href="#">Возможности</a></li>
-						<li><a href="#">Стоимость</a></li>
+						<li><a href="#">Отслеживание кодов</a></li>
+						<li><a href="#">Обработка Excel</a></li>
 					</ul>
 				</div>
 			</div>	
 			<div class="col-md-4">
-				<div class="dd-log-in">
-					<a href="#">Регистрация</a>
-					<a href="#">Личный кабинет</a>
+				<div class="dd-log-in dd-log-in-processing">
+					<a href="#">Профиль</a>
+					<a href="#">Выйти</a>
 				</div>
 			</div>
 			<div class="clearfix"></div>
