70fb392d8adcdd49f8b30a73d04bfa71530b4048
unknown <anzarsh@mail.ru>
2016-12-01 17:02:46 +0300

    userprofile.php 3

---
+++ includes/footer-userprofile.php
@@ -0,0 +1,42 @@
+<section class="dd-footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8">
+				<div class="footer-logo">
+					<div class="dd-img">
+						<img src="img/foot-l.png">
+					</div>
+					<div class="dd-logo-txt">
+						<span>Быстрый подбор кода <span>Тн Вэд Еаэс</span></span>
+					</div>
+				</div>
+				<div class="dd-footer-txt dd-footer-txt2">
+					<ul>
+						<li><a href="#">Что такое КаргоКод?</a></li>
+						<li><a href="#">Кому нужен КаргоКод</a></li>
+					</ul>
+				</div>
+				<div class="dd-footer-txt">
+					<ul>
+						<li><a href="#">Возможности</a></li>
+						<li><a href="#">Стоимость</a></li>
+					</ul>
+				</div>
+			</div>	
+			<div class="col-md-4">
+				<div class="dd-log-in">
+					<a href="#">Регистрация</a>
+					<a href="#">Личный кабинет</a>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+			<div class="col-md-12">
+				<div class="dd-oferta">
+				<a href="#">Договор-оферта</a>
+				<a href="#">Обратная связь</a>
+				</div>
+			</div>
+			<div class="clearfix"></div>			
+		</div>
+	</div>
+</section>
