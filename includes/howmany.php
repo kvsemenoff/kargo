17de1af9c88021aa82c05f5a21d628f320472cce
Barasbi <betokov93@gmail.com>
2016-12-01 16:59:27 +0300

    add howmoney

--- includes/howmany.php
+++ includes/howmany.php
@@ -3,12 +3,12 @@
 		<div class="db-all">
 			<h2>Сколько стоит КаргоКод?</h2>
 			<div class="db-all_box">
-				<div class="db-check_box">
+				<div class="db-check_box db-check_box_2">
 					<div class="db-check_box-one">
 						<img src="img/db-check.png" alt="check">
 					</div>
 					<div class="db-check_box-two">
-						Доступ к функции подбора кода ТН ВЭД ЕАЭС по описанию и получение актуальной ставки по коду
+						Доступ к функции подбора кода ТН ВЭД ЕАЭС по описанию и получение <br> актуальной ставки по коду
 						
 					</div>
 					<div class="clearfix"></div>
@@ -19,7 +19,7 @@
 						<img src="img/db-check.png" alt="check">
 					</div>
 					<div class="db-check_box-two">
-						Оперативное отслеживание изменений кодов ТН ВЭД ЕАЭС и актуальных ставок по найденным кодам
+						Оперативное отслеживание изменений кодов ТН ВЭД ЕАЭС и актуальных <br> ставок по найденным кодам
 					</div>
 					<div class="clearfix"></div>
 					<hr>
@@ -29,10 +29,40 @@
 						<img src="img/db-check.png" alt="check">
 					</div>
 					<div class="db-check_box-two">
-						Оперативное отслеживание изменений кодов ТН ВЭД ЕАЭС и актуальных ставок
+						Оперативное отслеживание изменений кодов ТН ВЭД ЕАЭС <br> и актуальных ставок
 					</div>
 					<div class="clearfix"></div>
 				</div>
+				<div class="db-all_box_days">
+					<div class="db-box-all_days">
+						<div class="db-all_days">
+							10 ДНЕЙ
+						</div>
+						<div class="db-all_money">
+							<span>1 500 РУБЛЕЙ</span>
+						</div>
+					</div>
+					<div class="db-box-all_days">
+						<div class="db-all_days">
+							30 ДНЕЙ
+						</div>
+						<div class="db-all_money">
+							<span>4 500 РУБЛЕЙ</span>
+						</div>
+					</div>
+					<div class="db-box-all_days">
+						<div class="db-all_days db-all_last">
+							3 МЕСЯЦА
+						</div>
+						<div class="db-all_money db-all_last">
+							<span>12 000 РУБЛЕЙ<span>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+			<div class="db-all-button">
+					<a href="#" class="button1">ПОЛЬЗУЙТЕСЬ БЕСПЛАТНО 30 ДНЕЙ</a>
 			</div>
 		</div>
 		<div class="clearfix"></div>
