a3ec6fcd665d098c182b29d47b18bac7825f6191
Berzeg07 <berezg86@gmail.com>
2016-12-01 10:17:10 +0300

    29.11

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <section class="dd-header">
 	<div class="main-container">
 		<div class="dd-logo">
-			<img src="img/dd-logo.png">
+			<a href="#"><img src="img/dd-logo.png"></a>
 		</div>
 		<div class="dd-main-menu hidden-xs hidden-sm">
 			<ul>
@@ -15,6 +15,13 @@
 			<a href="#">Регистрация</a>
 			<a href="#">Войти</a>
 		</div>
+		<div class="clearfix"></div>
+		<div class="dd-slogan">
+			<span>КаргоКод -<span> быстрый подбор кода</span><span class="dd-2"> Тн Вэд Еаэс</span></span>
+		</div>
+		<div class="dd-button-header">
+			<a class="button1" href="#">Попробовать бесплатно</a>
+		</div>	
 	</div>
 </section>
 
@@ -25,7 +32,7 @@
 	    <label class="anz-menu-ch hidden-lg hidden-md" for="check_1"><i class="fa fa-bars" aria-hidden="true"></i><img src="img/menu-ico.png"></label>
 	    <div class="anz-perspective">
 	        <div class="menu-list-pressed">
-	            <label class="anz-menu-ch1" for="check_1"><i class="fa fa-times" aria-hidden="true"></i></label>
+	            <label class="anz-menu-ch1" for="check_1"><i class="fa fa-times" aria-hidden="true"></i><img src="img/kres.png"></label>
 	            <ul class="dd-mainmenu">
 					<li><a href="#">Что такое карго код?</a></li>
 					<li><a href="#">Кому нужен карго код?</a></li>
