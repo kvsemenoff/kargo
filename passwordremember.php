64e9a08a8f545a48c9d99fa740cad181b419c4ef
Berzeg07 <berezg86@gmail.com>
2016-12-01 16:49:16 +0300

    29.11

--- passwordremember.php
+++ passwordremember.php
@@ -32,7 +32,47 @@
 	<script src="js/jquery.maskedinput.min.js"></script>
 	<script src="js/common.js"></script>
 		
-	<?php require_once('includes/header.php'); ?>
+	<section class="dd-header dd-header-pass-remember">
+	<div class="main-container">
+		<div class="dd-logo">
+			<a href="#"><img src="img/proc-logo.png"></a>
+		</div>
+		<div class="dd-main-menu hidden-xs hidden-sm">
+			<ul>
+				<li><a href="#">Что такое карго код?</a></li>
+				<li><a href="#">Кому нужен карго код?</a></li>
+				<li><a href="#">Возможности</a></li>
+				<li><a href="#">Стоимость</a></li>
+			</ul>
+		</div>
+		<div class="dd-log-in">
+			<a href="#">Регистрация</a>
+			<a href="#">Войти</a>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</section>
+
+
+
+<div class="anz-menu">
+	    <input type="checkbox" id="check_1" class=""/>
+	    <label class="anz-menu-ch hidden-lg hidden-md" for="check_1"><i class="fa fa-bars" aria-hidden="true"></i><img src="img/menu-ico.png"></label>
+	    <div class="anz-perspective">
+	        <div class="menu-list-pressed">
+	            <label class="anz-menu-ch1" for="check_1"><i class="fa fa-times" aria-hidden="true"></i><img src="img/kres.png"></label>
+	            <ul class="dd-mainmenu">
+					<li><a href="#">Что такое карго код?</a></li>
+					<li><a href="#">Кому нужен карго код?</a></li>
+					<li><a href="#">Возможности</a></li>
+					<li><a href="#">Стоимость</a></li>
+				</ul>
+	        </div>
+	    </div>
+	</div>
+</div>
+
+
 	<!-- Вспомнить пароль -->
 	<?php require_once('includes/passwordremember.php'); ?>
 
