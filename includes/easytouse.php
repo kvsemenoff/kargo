d5d2ce1da822a17eb9f50829a8bc33a856733f30
Barasbi <betokov93@gmail.com>
2016-12-02 11:01:16 +0300

    add easytouse.php

--- includes/easytouse.php
+++ includes/easytouse.php
@@ -1,1 +1,20 @@
-Удобно пользоваться
+<section class="db-easytouse">
+	<div class="container">
+		<div class="row">
+			<div class="db-easy_box">
+				<div class="db-easy_h2">
+					<h2>КаргоКодом удобно пользоваться</h2>			
+				</div>
+				<div class="db-easy_img">
+					<img src="img/db-smart.jpg" alt="browsers" class="img-responsive">
+				</div>
+				<div class="db-easy_p">					
+					<p>Поддерживается большинство устройств, операционных систем и браузеров</p>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+		
+
+</section>
