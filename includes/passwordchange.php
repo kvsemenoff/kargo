4787c923b0921138063eb8002e4cd218c497f90c
engine77 <zaur.ka.00@mail.ru>
2016-12-02 11:35:51 +0300

    1

--- includes/passwordchange.php
+++ includes/passwordchange.php
@@ -1,1 +1,29 @@
-Смена пароля
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="df-password-change">				
+			
+				<div class="df-caption-top">Смена пароля пользователя <span>testuser</span></div>
+					<div class="df-bg-color">
+						<div class="df-inputs">
+							<span>Введите новый пароль:</span>
+							<input type="password" value="********" class="input-class">
+						</div>
+						<div class="df-inputs">
+							<span>Подтвердите новый пароль:</span>
+							<input type="password" value="********" class="input-class df2">
+						</div>
+						<div class="df-buttons">
+							<a href="#" class="button1">
+								Изменить пароль
+							</a>
+							<a href="#" class="button1 df-but">
+								Отменить
+							</a>
+						</div>
+					</div>
+			
+			</div>
+		</div>
+	</div>
+</div>			
