8edb9dd96d7a4a47b5ecc1aa28bc5586ae0ca558
Berzeg07 <berezg86@gmail.com>
2016-12-07 13:58:22 +0300

    29.14

--- includes/header-processing.php
+++ includes/header-processing.php
@@ -5,75 +5,75 @@
 		</div>
 		<div class="dd-main-menu hidden-xs hidden-sm
 		">
-			<ul>
+		<ul>
+			<li><a href="#">Главная</a></li>
+			<li><a href="#">Подбор кода</a></li>
+			<li><a href="#">История запросов</a></li>
+			<li><a href="#">Отслеживание кодов</a></li>
+			<li><a href="#">Обработка Excel</a></li>
+		</ul>
+		<div class="dd-log-info">
+			<span>Логин: <span> testuser</span></span>
+			<span class="dd-pleft">Остаток: <span> 12 дней</span></span>
+			<a class="dd-pleft" href="#">Продлить КаргоКод</a>
+		</div>
+	</div>
+	<div class="dd-log-in">
+		<a href="#">Профиль</a>
+		<a href="#">Выйти</a>
+	</div>
+	<div class="clearfix"></div>
+	<div class="dd-obrabotka-wrap">
+		<div class="dd-title">
+			<span>Обработка файлов Excel</span>
+		</div>
+		<div class="dd-bg">
+			<form class="dd-form">
+				<div class="dd-name">
+					<span>Загрузите файл Excel:</span>
+				</div>
+				<div class="dd-input">
+					<input class="input-class" type="text" name="">
+					<input class="dd-submit" type="submit" value="Обзор..." name="">
+				</div>
+				<div class="dd-butt">
+					<a class="button1" href="#">Обработать файл</a>
+				</div>
+			</form>	
+		</div>
+		<div class="dd-result">
+			<span>21.11.2016 18:17  Файл загружен...</span>
+			<span>21.11.2016 18:17  Проверка корректности формата файла...</span>
+			<span>21.11.2016 18:17  Файл поставлен в очередь на обработку...</span>
+			<span>21.11.2016 18:18  Файл обрабатывается....</span>
+			<span class="dd-c-w">21.11.2016 18:19  <span class="dd-c">Файл обработан.</span></span>
+		</div>
+	</div>
+</div>
+</section>
+<div class="proof2"></div>
+
+
+<div class="anz-menu">
+	<input type="checkbox" id="check_1" class=""/>
+	<label class="anz-menu-ch hidden-lg hidden-md" for="check_1"><i class="fa fa-bars" aria-hidden="true"></i><img src="img/menu-ico.png"></label>
+	<div class="anz-perspective">
+		<div class="menu-list-pressed">
+			<label class="anz-menu-ch1" for="check_1"><i class="fa fa-times" aria-hidden="true"></i><img src="img/kres.png"></label>
+			<ul class="dd-mainmenu">
 				<li><a href="#">Главная</a></li>
 				<li><a href="#">Подбор кода</a></li>
 				<li><a href="#">История запросов</a></li>
 				<li><a href="#">Отслеживание кодов</a></li>
 				<li><a href="#">Обработка Excel</a></li>
 			</ul>
-			<div class="dd-log-info">
+			<div class="dd-log-info2">
 				<span>Логин: <span> testuser</span></span>
-				<span class="dd-pleft">Остаток: <span> 12 дней</span></span>
-				<a class="dd-pleft" href="#">Продлить КаргоКод</a>
-			</div>
-		</div>
-		<div class="dd-log-in">
-			<a href="#">Профиль</a>
-			<a href="#">Выйти</a>
-		</div>
-		<div class="clearfix"></div>
-		<div class="dd-obrabotka-wrap">
-			<div class="dd-title">
-				<span>Обработка файлов Excel</span>
-			</div>
-			<div class="dd-bg">
-				<form class="dd-form">
-					<div class="dd-name">
-						<span>Загрузите файл Excel:</span>
-					</div>
-					<div class="dd-input">
-						<input class="input-class" type="text" name="">
-						<input class="dd-submit" type="submit" value="Обзор..." name="">
-					</div>
-					<div class="dd-butt">
-						<a class="button1" href="#">Обработать файл</a>
-					</div>
-				</form>	
-			</div>
-			<div class="dd-result">
-				<span>21.11.2016 18:17  Файл загружен...</span>
-				<span>21.11.2016 18:17  Проверка корректности формата файла...</span>
-				<span>21.11.2016 18:17  Файл поставлен в очередь на обработку...</span>
-				<span>21.11.2016 18:18  Файл обрабатывается....</span>
-				<span class="dd-c-w">21.11.2016 18:19  <span class="dd-c">Файл обработан.</span></span>
+				<span >Остаток: <span> 12 дней</span></span>
+				<a href="#">Продлить КаргоКод</a>
 			</div>
 		</div>
 	</div>
-</section>
-<div class="proof2"></div>
-
-
-<div class="anz-menu">
-	    <input type="checkbox" id="check_1" class=""/>
-	    <label class="anz-menu-ch hidden-lg hidden-md" for="check_1"><i class="fa fa-bars" aria-hidden="true"></i><img src="img/menu-ico.png"></label>
-	    <div class="anz-perspective">
-	        <div class="menu-list-pressed">
-	            <label class="anz-menu-ch1" for="check_1"><i class="fa fa-times" aria-hidden="true"></i><img src="img/kres.png"></label>
-	            <ul class="dd-mainmenu">
-	            	<li><a href="#">Главная</a></li>
-	            	<li><a href="#">Подбор кода</a></li>
-	            	<li><a href="#">История запросов</a></li>
-	            	<li><a href="#">Отслеживание кодов</a></li>
-	            	<li><a href="#">Обработка Excel</a></li>
-	            </ul>
-	            <div class="dd-log-info2">
-	            	<span>Логин: <span> testuser</span></span>
-	            	<span >Остаток: <span> 12 дней</span></span>
-	            	<a href="#">Продлить КаргоКод</a>
-	            </div>
-	        </div>
-	    </div>
-	</div>
+</div>
 </div>
 
